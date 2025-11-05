cf3b6f90aa6732be2c55c52f26dacd71a6423504
Ryohei Kamei <231205751@ccmailg.meijo-u.ac.jp>
2025-11-05 11:45:18 +0900

    fix:format

--- backend/src/db/database.ts
+++ backend/src/db/database.ts
@@ -3,6 +3,7 @@ export type Bindings = {
 	ASSET_BUCKET: R2Bucket;
 };
 
-export const dbConnect = (env: Bindings): D1Database => env.advanced_project_team2;
+export const dbConnect = (env: Bindings): D1Database =>
+	env.advanced_project_team2;
 
 export type Database = ReturnType<typeof dbConnect>;
