27fb1678142f6fb3314c3f9b696a29f8c568df2a
Ryohei Kamei <231205751@ccmailg.meijo-u.ac.jp>
2025-11-19 17:04:15 +0900

    feat:environments

--- backend/src/db/database.ts
+++ backend/src/db/database.ts
@@ -3,6 +3,10 @@ export type Bindings = {
 	ASSET_BUCKET: R2Bucket;
 	FRONTEND_ORIGIN?: string;
 	YAHOO_MAPS_API_KEY: string;
+	R2_BUCKET_NAME: string;
+	CLOUDFLARE_R2_ACCOUNT_ID: string;
+	R2_ACCESS_KEY_ID: string;
+	R2_SECRET_ACCESS_KEY: string;
 };
 
 export const dbConnect = (env: Bindings): D1Database =>
